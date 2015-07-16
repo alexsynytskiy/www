a35d4ccda1648b4fe935d52e8f47c2c975389a72
Dynamomania <alexsynytskiy@ukr.net>
2015-07-16 13:35:43 +0300

    Local DB name changed

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=summertest',
+            'dsn' => 'mysql:host=localhost;dbname=myisam_dynamo',
             'emulatePrepare' => true,
             'username' => 'root',
             'password' => '',
