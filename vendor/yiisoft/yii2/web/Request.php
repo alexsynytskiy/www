9423ac0ba5b9c35123075621dcb571ac795dbda8
pro100olgert <olgert.vaskevich@gmail.com>
2015-08-13 11:58:02 +0300

    Added the ip address view

--- vendor/yiisoft/yii2/web/Request.php
+++ vendor/yiisoft/yii2/web/Request.php
@@ -852,7 +852,14 @@ class Request extends \yii\base\Request
      */
     public function getUserIP()
     {
-        return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
 
     /**
